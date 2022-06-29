bdfeca63ccfd41f2fc84fe29c46f825ee1dfb948
AhasanulWorkstation <ahasanular@gmail.com>
2022-06-29 08:40:10 +0600

    set post list orderby

--- posts/admin.py
+++ posts/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import Posts, PostLikes
 
+
 class PostsAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         query_set = Posts.objects.filter(is_archived=False).all().order_by('id')
         return query_set
 
-    fields = ['user', 'post_by', 'text', 'img', 'is_archived']
-    list_display = ['id', 'user', 'is_archived', 'created_at', 'modified_at', 'archived_at']
+    fields = ['user', 'student', 'teacher', 'post_by', 'text', 'img', 'is_archived']
+    list_display = ['id', 'user', 'student', 'teacher', 'is_archived', 'created_at', 'modified_at', 'archived_at']
+
 
 admin.site.register(Posts, PostsAdmin)
+
 
 class PostLikesAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
@@ -18,5 +21,6 @@ class PostLikesAdmin(admin.ModelAdmin):
 
     fields = ['user', 'post']
     list_display = ['user', 'post', 'is_archived', 'created_at']
+
 
 admin.site.register(PostLikes, PostLikesAdmin)
